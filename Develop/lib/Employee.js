bbd85591f6cce791816401523e2b7868166264a2
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-06-08 11:58:54 -0700

    Write and add employee.js code for the employee.test.js functions

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,5 +1,5 @@
 // TODO: Write code to define and export the Employee class
-
+const inquirer = require("inquirer");
 class Employee {
   constructor(name, id, email) {
     this.name = name;
@@ -7,5 +7,37 @@ class Employee {
     this.email = email;
   }
 }
+
+function getName() {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?",
+      },
+    ])
+    .then((answers) => {
+      this.name = answers.name;
+      this.getName();
+    });
+}
+
+function getId() {
+  return inquirer
+    .prompt([
+      {
+        type: "number",
+        name: "id",
+        message: "What is the employee's ID?",
+      },
+    ])
+    .then((answers) => {
+      this.id = answers.id;
+      this.getId();
+    });
+}
+
+const e = new Employee();
 
 module.exports = Employee;
--- Develop/test/Employee.test.js
+++ Develop/test/Employee.test.js
@@ -27,7 +27,7 @@ test("Can get name via getName()", () => {
   const testValue = "Alice";
   const e = new Employee(testValue);
   expect(e.getName()).toBe(testValue);
-});
+}); // New structure will be needed here
 
 test("Can get id via getId()", () => {
   const testValue = 100;
