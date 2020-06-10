ce1889c5db7d36082ae1f8315e651089d9436705
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-06-09 21:36:10 -0700

    Update last complete comment

--- Develop/test/Engineer.test.js
+++ Develop/test/Engineer.test.js
@@ -4,13 +4,13 @@ test("Can set GitHUb account via constructor", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
   expect(e.github).toBe(testValue);
-}); // Complete
+});
 
 test('getRole() should return "Engineer"', () => {
   const testValue = "Engineer";
   const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
   expect(e.getRole()).toBe(testValue);
-});
+}); // Complete
 
 test("Can get GitHub username via getGithub()", () => {
   const testValue = "GitHubUser";
